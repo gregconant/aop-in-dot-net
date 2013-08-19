3e68bbe745f6305837c8e664683eeaf936879e66
Greg Conant <greg.conant@gmail.com>
2013-08-19 09:58:06 -0400

    gc - Interception Aspect using PostSharp

---
+++ TweetPostSharp/TweetPostSharp/MyInterceptorAspect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PostSharp.Aspects;
+
+namespace TweetPostSharp {
+  [Serializable]
+  public class MyInterceptorAspect : MethodInterceptionAspect {
+    public override void OnInvoke(MethodInterceptionArgs args) {
+      Console.WriteLine("Interceptor 1");
+      args.Proceed(); // this calls the intercepted method
+      Console.WriteLine("Interceptor 2");
+    }
+  }
+}
--- TweetPostSharp/TweetPostSharp/Program.cs
+++ TweetPostSharp/TweetPostSharp/Program.cs
@@ -4,8 +4,19 @@ using System.Linq;
 using System.Text;
 
 namespace TweetPostSharp {
+  
+  public class TwitterClient {
+    [MyInterceptorAspect]
+    public void Send(string msg) {
+      Console.WriteLine("Sending: {0}", msg);
+    }
+  }
+
   class Program {
     static void Main(string[] args) {
+      var svc = new TwitterClient();
+      svc.Send("hi");
+      Console.ReadKey();
     }
   }
 }
