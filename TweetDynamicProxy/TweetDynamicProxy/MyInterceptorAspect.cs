c91075f470cd0abea580ba6052daeff498a4ecb2
Greg Conant <greg.conant@gmail.com>
2013-08-19 10:19:04 -0400

    gc - method interception with Castle DynamicProxy

---
+++ TweetDynamicProxy/TweetDynamicProxy/MyInterceptorAspect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+
+namespace TweetDynamicProxy {
+  public class MyInterceptorAspect : IInterceptor {
+    public void Intercept(IInvocation invocation) {
+      Console.WriteLine("Interceptor Message 1");
+      invocation.Proceed();
+      Console.WriteLine("Interceptor Message 2");
+
+    }
+  }
+}
---
+++ TweetDynamicProxy/TweetDynamicProxy/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+
+namespace TweetDynamicProxy {
+  public class TwitterClient {
+    public virtual void Send(string msg) {
+      // note that this method was made Virtual. This is necessary for
+      // DynamicProxy interception to work. (At least when intercepting
+      // a concrete class. Not necessary for interfaces.)
+      Console.WriteLine("Sending: {0}", msg);
+    }  
+
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      var proxyGenerator = new ProxyGenerator();
+      //var svc = new TwitterClient();
+      var svc = proxyGenerator
+        .CreateClassProxy<TwitterClient>(new MyInterceptorAspect());
+
+      svc.Send("hi");
+      Console.ReadKey();
+    }
+  }
+}
