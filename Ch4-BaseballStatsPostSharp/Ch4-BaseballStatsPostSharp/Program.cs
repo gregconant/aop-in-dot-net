0f1ecabff03c4fd093fb6e96c19bd8320d7f4326
Greg Conant <greg.conant@gmail.com>
2013-08-19 13:00:36 -0400

    gc - basic baseball stats - no aspects yet

---
+++ Ch4-BaseballStatsPostSharp/Ch4-BaseballStatsPostSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch4_BaseballStatsPostSharp {
+
+  public class BaseballStats {
+    public decimal GetBattingAverage(string playerName) {
+      if (playerName == "Jimmy Rollins") {
+        return 0.248M;
+      }
+      if (playerName == "Ryan Howard") {
+        return 0.266M;
+      }
+      if (playerName == "Chase Utley") {
+        return 0.277M;
+      }
+      return 0.000M;
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      var stats = new BaseballStats();
+      var player = "Jimmy Rollins";
+      var battingAvg = stats.GetBattingAverage(player);
+      Console.WriteLine("{0}'s Batting Average: {1}", player, battingAvg.ToString());
+
+      Console.ReadKey();
+    }
+  }
+}
