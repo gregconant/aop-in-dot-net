66b909f5556982ca5c54527cfbd207952cb85685
Greg Conant <greg.conant@gmail.com>
2013-08-26 11:01:02 -0400

    gc - basic HttpModule with BeginRequest and EndRequest handling

---
+++ Ch4-AspNetHttpModuleDemo/AspNetHttpModuleDemo/Modules/MyHttpModule.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetHttpModuleDemo.Modules {
+  public class MyHttpModule : IHttpModule {
+    public void Dispose() {
+      throw new NotImplementedException();
+    }
+
+    public void Init(HttpApplication context) {
+      context.BeginRequest += new EventHandler(context_BeginRequest);
+      context.EndRequest += new EventHandler(context_EndRequest);
+    }
+    
+    void context_BeginRequest(object sender, EventArgs e) {
+      var app = (HttpApplication) sender;
+      app.Response.Write("Before the page is requested");
+    }
+
+    void context_EndRequest(object sender, EventArgs e) {
+      var app = (HttpApplication) sender;
+      app.Response.Write("After the page is handled");
+    }
+
+  }
+}
