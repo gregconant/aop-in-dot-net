35d2bb950364b5ac45dea142e23f0c8b806f40ee
Greg Conant <greg.conant@gmail.com>
2013-08-27 19:15:05 -0400

    gc - Chapter 4 - Car Value slow service call, no caching

---
+++ Ch4-AspNetCaching/Ch4-AspNetCaching/CarValueService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading;
+
+namespace Ch4_AspNetCaching {
+  public class CarValueService {
+    readonly Random _rand;
+
+    public CarValueService() {
+      _rand = new Random();
+    }
+
+    public decimal GetValue(int makeId, int conditionId, int year) {
+      Thread.Sleep(5000);
+      return _rand.Next(10000, 20000);
+    }
+  }
+}
