ca55b0e2aab01a9a92b741f5c205bcadd2d8fbfc
Greg Conant <greg.conant@gmail.com>
2013-08-27 19:43:47 -0400

    gc - basic cache key setup. cannot test on this workstation due to licensing issues.

--- Ch4-AspNetCaching/Ch4-AspNetCaching/CarValueService.cs
+++ Ch4-AspNetCaching/Ch4-AspNetCaching/CarValueService.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Threading;
+using PostSharp.Aspects;
 
 namespace Ch4_AspNetCaching {
+
+  public class CarValueCacheAspect : OnMethodBoundaryAspect {
+    public override void OnSuccess(MethodExecutionArgs args) {
+      var key = GetCacheKey(args).ToString();
+      HttpContext.Current.Cache[key] = args.ReturnValue;
+    }
+
+    private object GetCacheKey(MethodExecutionArgs args) {
+      // to get a unique key, we join all arguments into one string
+      var concatArguments = string.Join("_", args.Arguments);
+      concatArguments = args.Method.Name + "_" + concatArguments;
+      return concatArguments;
+    }
+
+  }
+
   public class CarValueService {
     readonly Random _rand;
 
@@ -12,6 +29,7 @@ namespace Ch4_AspNetCaching {
       _rand = new Random();
     }
 
+    [CarValueCacheAspect]
     public decimal GetValue(int makeId, int conditionId, int year) {
       Thread.Sleep(5000);
       return _rand.Next(10000, 20000);
