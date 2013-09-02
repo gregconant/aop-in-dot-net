1cf2b37232b4733141e25f4d9615c3d645e5dc36
Greg Conant <greg.conant@gmail.com>
2013-09-02 17:07:20 -0400

    gc - adding basic example of pointcut tests for MVC project (controller action has particular attribute)

---
+++ Chapter6-01-UnitTestingAspects/Chapter6-01-UnitTestingAspects.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Chapter6_01_UnitTestingAspects;
+using Chapter6_01_UnitTestingAspects.Controllers;
+using NUnit.Framework;
+
+namespace Chapter6_01_UnitTestingAspects.Tests.Controllers {
+  [TestFixture]
+  public class HomeControllerTest {
+    [Test]
+    public void Index() {
+      // Arrange
+      HomeController controller = new HomeController();
+
+      // Act
+      ViewResult result = controller.Index() as ViewResult;
+
+      // Assert
+      Assert.AreEqual("Welcome to ASP.NET MVC!", result.ViewBag.Message);
+    }
+
+    [Test]
+    public void About() {
+      // Arrange
+      HomeController controller = new HomeController();
+
+      // Act
+      ViewResult result = controller.About() as ViewResult;
+
+      // Assert
+      Assert.IsNotNull(result);
+    }
+  }
+}
