d9f3bfe45760e38db354b3cd58088e01bbca33ee
Greg Conant <greg.conant@gmail.com>
2013-09-02 17:53:07 -0400

    gc - replacing newed-up objects with call to StructureMap

--- Chapter6-03-CastleAspectTestingWithDependencies/Chapter6-03-CastleAspectTestingWithDependencies/Program.cs
+++ Chapter6-03-CastleAspectTestingWithDependencies/Chapter6-03-CastleAspectTestingWithDependencies/Program.cs
@@ -2,12 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using StructureMap;
 
 namespace Chapter6_03_CastleAspectTestingWithDependencies {
   class Program {
     static void Main(string[] args) {
-      var myOtherObj = new MyOtherService();
-      var myObj = new MyService(myOtherObj);
+
+      // Here, StructureMap figures out our dependencies.
+      ObjectFactory.Initialize(x =>
+        {
+          x.Scan(scan => {
+            scan.TheCallingAssembly();
+            scan.WithDefaultConventions();
+          });
+        });
+      
+      var myObj = ObjectFactory.GetInstance<IMyService>();
       myObj.DoSomething();
 
       Console.ReadKey();
