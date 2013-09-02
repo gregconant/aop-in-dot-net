c419514ea74289d08e2140b4cf7b1dfcb5896305
Greg Conant <greg.conant@gmail.com>
2013-09-02 18:24:08 -0400

    gc - basic test of PostSharp aspect using DynamicMethod

---
+++ Chapter6-04-PostSharpAspectTesting/Chapter6-04-PostSharpAspectTesting/MyBoundaryAspect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PostSharp.Aspects;
+
+namespace Chapter6_04_PostSharpAspectTesting {
+  [Serializable]
+  public class MyBoundaryAspect : OnMethodBoundaryAspect {
+    public override void OnEntry(MethodExecutionArgs args) {
+      Log.Write("Before: " + args.Method.Name);
+    }
+    public override void OnSuccess(MethodExecutionArgs args) {
+      Log.Write("Success: " + args.Method.Name);
+    }
+  }
+}
