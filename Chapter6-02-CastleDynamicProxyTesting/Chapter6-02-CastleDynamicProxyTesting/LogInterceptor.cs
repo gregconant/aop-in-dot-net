b78193c66af0c1e715c307793209bcd4f61b1dbc
Greg Conant <greg.conant@gmail.com>
2013-09-02 17:26:10 -0400

    gc - basic test of Castle DynamicProxy aspect

---
+++ Chapter6-02-CastleDynamicProxyTesting/Chapter6-02-CastleDynamicProxyTesting/LogInterceptor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Castle.DynamicProxy;
+
+namespace Chapter6_02_CastleDynamicProxyTesting {
+  public class LogInterceptor : IInterceptor {
+
+    public void Intercept(IInvocation invocation) {
+      var methodName = invocation.Method.Name;
+      Log.Write("Before " + methodName);
+      invocation.Proceed();
+      Log.Write("After " + methodName);
+    }
+  }
+}
---
+++ Chapter6-02-CastleDynamicProxyTesting/Chapter6-02-CastleDynamicProxyTesting/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter6_02_CastleDynamicProxyTesting {
+  class Program {
+    static void Main(string[] args) {
+    }
+  }
+}
