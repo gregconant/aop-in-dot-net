791c561b2b7463f0e72f53d5fd59b30cd82c624f
Greg Conant <greg.conant@gmail.com>
2013-09-02 21:48:18 -0400

    gc - adding last example of PostSharp aspect unit testing

---
+++ Chapter6-05-PostSharpTestingWithDI/Chapter6-05-PostSharpTestingWithDI/MyLoggingAspectTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using PostSharp.Aspects;
+using StructureMap;
+
+namespace Chapter6_05_PostSharpTestingWithDI {
+  [TestFixture]
+  public class MyLoggingAspectTest {
+
+    [Test]
+    public void Intercept() {
+      var mockLoggingService = new Mock<ILoggingService>();
+      var args = new MethodExecutionArgs(null, Arguments.Empty);
+
+      ObjectFactory.Initialize(x => 
+        x.For<ILoggingService>()
+        .Use(mockLoggingService.Object));
+
+      var sut = new MyLoggingAspect();
+      // normally PostSharp runs this for us, but that only happens at
+      // runtime, so we have to call it manually.
+      sut.RuntimeInitialize(null);
+
+      sut.OnEntry(args);
+      sut.OnSuccess(args);
+
+      mockLoggingService.Verify(s => s.Write("Log start"));
+      mockLoggingService.Verify(s => s.Write("Log end"));
+    }
+  }
+}
---
+++ Chapter6-05-PostSharpTestingWithDI/Chapter6-05-PostSharpTestingWithDI/MyThinAspectExample.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PostSharp.Aspects;
+using StructureMap;
+
+namespace Chapter6_05_PostSharpTestingWithDI {
+
+  public class MyNormalCode {
+    [MyThinAspect]
+    public string Reverse(string str) {
+      return new string(str.Reverse().ToArray());
+    }
+  }
+
+  public static class AspectSettings {
+    public static bool On = true;
+  }
+
+  [Serializable]
+  public class MyThinAspect : OnMethodBoundaryAspect {
+    private IMyCrossCuttingConcern _concern;
+
+    public override void RuntimeInitialize(System.Reflection.MethodBase method) {
+      if (!AspectSettings.On) {
+        return;
+      }
+      _concern = ObjectFactory.GetInstance<IMyCrossCuttingConcern>();
+    }
+
+    public override void OnEntry(MethodExecutionArgs args) {
+      if (!AspectSettings.On) {
+        return;
+      }
+      _concern.BeforeMethod("before");
+    }
+
+    public override void OnSuccess(MethodExecutionArgs args) {
+      if (!AspectSettings.On) {
+        return;
+      }
+      _concern.AfterMethod("after");
+    }
+  }
+
+  public interface IMyCrossCuttingConcern {
+    void BeforeMethod(string str);
+    void AfterMethod(string str);
+  }
+
+  public class MyCrossCuttingConcern : IMyCrossCuttingConcern {
+    private ILoggingService _logService;
+
+    public void BeforeMethod(string str) {
+      _logService.Write(str);
+    }
+
+    public void AfterMethod(string str) {
+      _logService.Write(str);
+    }
+
+  }
+}
